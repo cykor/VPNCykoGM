3edc686d1e5cb8cd85a5489c4e2f59d7fee59cc3
cykor <cykorr@gmail.com>
2012-10-30 12:49:31 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,15 +1,16 @@
 #nslookup domain and add its ip to ...
 IP=$(nslookup $1 | grep "Address 1: " | tail -n 1 | sed 's/^Address 1: //g' | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
+echo "Current DNS returns: $IP"
+
 TRUEIP=$(nslookup $1 8.8.4.4 | grep "Address 1: " | tail -n 1 | sed 's/^Address 1: //g' | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
+echo "Google DNS returns:  $TRUEIP"
 
-echo "Current DNS return: $IP"
-echo "Google DNS return:  $TRUEIP"
-
-echo "add server=/$1/8.8.4.4 to known_gfw_domains"
 if [ $IP \!= $TRUEIP ]; then
-	echo "add server=/$1/8.8.4.4 to gfwdomains"
+	echo "[!] add server=/$1/8.8.4.4 to dnsmasq"
 fi
 
+# TODO: add domain to known_gfw_domains
+# TODO: add route to vpnup.sh (and add route immediately)
 sed -i "1i#$1" test
 sed -i "2iroute add $TRUEIP via \$VPNGW Metric 5" test
 sed -i '3i\
