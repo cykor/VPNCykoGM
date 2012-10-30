a2b5ac2f079815f3cea8a53a714e7b3220ed5450
cykor <cykorr@gmail.com>
2012-10-30 16:25:04 +0800

    Working Version

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 sed -i "1i#$1\
 ' known_gfw_domains
-echo "added $1 to known_gfw_domains"
+echo "$1 added to known_gfw_domains"
 
 IP=$(nslookup $1 | grep "Address 1: " | tail -n 1 | sed 's/^Address 1: //g' | grep -o "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
 echo "Current DNS returns: $IP"
